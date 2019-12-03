88f455c940116a154e167b761afd1e8f84665842
James Weston <jweston@walmartlabs.com>
2019-12-03 09:38:54 -0500

    include players

--- components/slatepicker.js
+++ components/slatepicker.js
@@ -1,42 +1,66 @@
 import Link from 'next/link';
 import Card from './card';
+import fetch from 'isomorphic-unfetch';
+
 
 class SlatePicker extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
     this.state = {
-      selected: ''
-    }
+      slates: {},
+      players: {}
+    };
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleChange(event) {
-    this.setState({selected: event.target.value});
+  async handleChange(event) {
+    const draftGroupId = event.target.value;
+    this.setState({selected: draftGroupId});
   }
 
   render() {
-    const { props } = this;
-    props.slates && !this.state.selected && this.setState({ selected: props.slates[0].DraftGroupId });
+    let { props: { slates }, state: { selected } } = this;
+    const draftGroupIds = Object.keys(slates).map((key) => key);
+    if (!selected) {
+      selected = draftGroupIds[0];
+      this.setState({ selected });
+    }
 
+    const slate = selected && slates[selected];
+    console.log(slates)
     return (
       <Card>
         <h2 style={{ marginTop: 0 }}>Pick a slate</h2>
         <div>
           <select value={this.state.selected} onChange={this.handleChange}>
-            {props.slates && props.slates.map((slate) => {
-
-              const startTime = new Date((slate.StartDateEdt.substring(6, slate.StartDateEdt.length - 2) - 0));
-              const lineText = `${slate.SportDisplayName} ${slate.GameType.GameStyle.Name} at ${startTime} ${slate.ContestStartTimeSuffix}`;
-              return (
-                <option value={slate.DraftGroupId} key={slate.DraftGroupId}>{lineText}</option>
-              );
-            })}
+            {
+              draftGroupIds && draftGroupIds.map((draftGroupId) => {
+                const slate = slates[draftGroupId];
+                const startTime = new Date((slate.StartDateEdt.substring(6, slate.StartDateEdt.length - 2) - 0));
+                const lineText = `${slate.SportDisplayName} ${slate.GameType.GameStyle.Name} at ${startTime} ${slate.ContestStartTimeSuffix}`;
+                return (
+                  <option value={draftGroupId} key={draftGroupId}>{lineText}</option>
+                );
+              })
+            }
           </select>
         </div>
         <div>
           <button onClick={() => console.log(this.state.selected)}>Next</button>
         </div>
+        {
+          slate && (
+            <div>
+              <h3>Slate Info</h3>
+              <p>{slate.Sport} - {slate.GameType.Name}</p>
+              <h4>Players</h4>
+              <ul>
+                <li>{slate.players && slate.players.draftables && `${slate.players.draftables[0].displayName} - ${slate.players.draftables[0].salary}`}</li>
+              </ul>
+            </div>
+          )
+        }
       </Card>
     )
   }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import ImportProjection from '../components/importprojection.js';
 import SlatePicker from '../components/slatepicker';
 import fetch from 'isomorphic-unfetch';
 
+const slates = {};
+
 const Index = props => (
   <div>
     <Header />
@@ -15,14 +17,35 @@ const Index = props => (
   </div>
 );
 
-Index.getInitialProps = async function() {
+const getPlayers = async function (slate) {
 
+  const draftGroupId = slate.DraftGroupId;
+  if (!slates[draftGroupId]) {
+    //Handle errrrrsssds
+    console.log(`http://api.draftkings.com/draftgroups/v1/${draftGroupId}/draftables?format=json`)
+    const playersRes = await fetch(`http://api.draftkings.com/draftgroups/v1/draftgroups/${draftGroupId}/draftables?format=json`, { mode: 'no-cors' });
+
+    const players = await playersRes.json();
+
+    slates[draftGroupId] = {
+      ...slate,
+      players
+    };
+  }
+  console.log('got players')
+};
+
+Index.getInitialProps = async function() {
   //Handle errrrrs
   const res = await fetch('https://www.draftkings.com/lineup/getupcomingcontestinfo', { method: 'post', mode: 'no-cors' });
-  const slates = await res.json();
+  const rawSlates = await res.json();
+  const draftGroupIds = rawSlates.map((slate) => slate.draftGroupId);
+
+  await Promise.all(rawSlates.map(getPlayers));
+
   return {
     slates
-  };
+  }
 };
 
 export default Index;
