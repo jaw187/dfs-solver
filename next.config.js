3e9654abe6ad9c6a417b1f7b5f392fc14fa7a26a
James Weston <jweston@walmartlabs.com>
2020-01-03 09:26:30 -0500

    reset config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
 const withWorkers = require('@zeit/next-workers')
-module.exports = withWorkers({
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // your aliases
-    }
-  }
-})
+module.exports = withWorkers()
